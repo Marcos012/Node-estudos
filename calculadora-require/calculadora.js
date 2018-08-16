abf42921881fd97c8e0bf67f3f478a7f5b31e3de
marcos012 <marcospaulo.ad@gmail.com>
2018-08-16 13:23:05 -0300

    Estudos de rotar e require em node.js

---
+++ calculadora-require/calculadora.js
@@ -0,0 +1,14 @@
+somar = (num1, num2) => {
+  return num1 + num2
+}
+
+subtrair = (num1, num2) => {
+  return num1 - num2
+}
+
+const operacoes = {
+  somar,
+  subtrair
+}
+
+module.exports = operacoes
---
+++ calculadora-require/index.js
@@ -0,0 +1,4 @@
+const calculadora = require('./calculadora.js')
+
+console.log(calculadora.somar(1, 1))
+console.log(calculadora.subtrair(1, 1))
